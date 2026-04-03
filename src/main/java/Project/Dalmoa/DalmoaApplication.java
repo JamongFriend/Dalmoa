75e0ff9ce4c8b5850de025978eb8eb2938210be5
JamogFrinds <ghrb6769@gmail.com>
2026-04-03 12:14:02 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/Project/Dalmoa/DalmoaApplication.java
@@ -0,0 +1,13 @@
+package Project.Dalmoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalmoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalmoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Project/Dalmoa/DalmoaApplicationTests.java
@@ -0,0 +1,13 @@
+package Project.Dalmoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalmoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
